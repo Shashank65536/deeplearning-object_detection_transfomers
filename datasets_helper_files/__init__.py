f0f3a85857fcdd48964b3cdd36e3087b3bfdd744
unknown <shashankbidwai6@gmail.com>
2022-12-17 23:56:17 -0500

    initial commit

---
+++ datasets_helper_files/__init__.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# ------------------------------------------------------------------------
+import torch.utils.data
+import torchvision
+from .build_dataset import build as build_corals
+
+
+def build_dataset(image_set, args):
+    return build_corals(image_set,args)
+
+def get_coco_api_from_dataset(dataset):
+    for _ in range(10):
+        # if isinstance(dataset, torchvision.datasets.CocoDetection):
+        #     break
+        if isinstance(dataset, torch.utils.data.Subset):
+            dataset = dataset.dataset
+    if isinstance(dataset, torchvision.datasets.CocoDetection):
+        return dataset.coco
